c1bf0938e9f68dad77534e358bfc9d06f8408d1a
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-05-20 15:13:34 +0200

    updated max scripts 4.0.4

--- scripts/fourohandbeyond/mac/environment.sh
+++ scripts/fourohandbeyond/mac/environment.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME=$(pwd)/zulu11.37.17-ca-jdk11.0.6-macosx_x64
+export JAVA_HOME=$(pwd)/zulu11.39.15-ca-jdk11.0.7-macosx_x64
 export PATH=$JAVA_HOME/bin:$PATH
--- scripts/fourohandbeyond/mac/unpack.sh
+++ scripts/fourohandbeyond/mac/unpack.sh
@@ -1,3 +1,4 @@
-tar -xzf install/zulu11.37.17-ca-jdk11.0.6-macosx_x64.tar.gz
-tar -xzf install/neo4j-enterprise-4.0.3-unix.tar.gz
-mv install/apoc-4.0.0.5-all.jar neo4j-enterprise-4.0.3/plugins
+tar -xzf install/zulu11.39.15-ca-jdk11.0.7-macosx_x64.tar.gz
+tar -xzf install/neo4j-enterprise-4.0.4-unix.tar.gz
+mv install/apoc-4.0.0.12-all.jar neo4j-enterprise-4.0.4/plugins
+mv install/apoc-nlp-dependencies-4.0.0.12.jar neo4j-enterprise-4.0.4/plugins
