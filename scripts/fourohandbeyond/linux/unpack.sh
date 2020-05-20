77ec8abac6ee4629360ff7bad7b8a357cb48e71c
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-05-20 14:29:17 +0200

    updated linux scripts 4.0.4

--- scripts/fourohandbeyond/linux/unpack.sh
+++ scripts/fourohandbeyond/linux/unpack.sh
@@ -1,3 +1,4 @@
 tar -xzf install/zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
 tar -xzf install/neo4j-enterprise-4.0.4-unix.tar.gz
-mv install/apoc-4.0.0.10-all.jar neo4j-enterprise-4.0.4/plugins
+mv install/apoc-4.0.0.12-all.jar neo4j-enterprise-4.0.4/plugins
+mv install/apoc-nlp-dependencies-4.0.0.12.jar neo4j-enterprise-4.0.4/plugins
