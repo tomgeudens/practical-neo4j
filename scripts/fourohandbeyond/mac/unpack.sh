82fb992c1d4fd90977e73b4bc8a76a060c7c3d46
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-06-22 15:26:23 +0200

    add -q parameter to unzip for 4.0.6

--- scripts/fourohandbeyond/mac/unpack.sh
+++ scripts/fourohandbeyond/mac/unpack.sh
@@ -1,6 +1,6 @@
-unzip ./install/zulu11.39.15-ca-jre11.0.7-macosx_x64.zip
+unzip -q ./install/zulu11.39.15-ca-jre11.0.7-macosx_x64.zip
 tar -xzf install/neo4j-enterprise-4.0.6-unix.tar.gz
-unzip ./install/neo4j-graph-data-science-1.2.2-standalone.zip -d ./install
+unzip -q ./install/neo4j-graph-data-science-1.2.2-standalone.zip -d ./install
 
 mv install/apoc-4.0.0.15-all.jar neo4j-enterprise-4.0.6/plugins
 mv install/apoc-nlp-dependencies-4.0.0.15.jar neo4j-enterprise-4.0.6/plugins
