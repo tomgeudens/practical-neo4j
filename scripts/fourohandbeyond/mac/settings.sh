f3bfb831efcecafaa314e7bfb64910bee72f3391
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-06-22 14:28:39 +0200

    update mac scripts 4.0.6

--- scripts/fourohandbeyond/mac/settings.sh
+++ scripts/fourohandbeyond/mac/settings.sh
@@ -1,14 +1,14 @@
 # changes to neo4j.conf file
-echo "metrics.enabled=false" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.security.procedures.unrestricted=apoc.*" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.default_listen_address=0.0.0.0" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.logs.query.enabled=off" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.memory.heap.initial_size=2048m" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.memory.heap.max_size=2048m" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
-echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-4.0.4/conf/neo4j.conf
+echo "metrics.enabled=false" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.security.procedures.unrestricted=apoc.*,gds.*" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.default_listen_address=0.0.0.0" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.logs.query.enabled=off" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.memory.heap.initial_size=2048m" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.memory.heap.max_size=2048m" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
+echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-4.0.6/conf/neo4j.conf
 
 # setting the initial password
-export JAVA_HOME=$(pwd)/zulu11.39.15-ca-jdk11.0.7-macosx_x64
+export JAVA_HOME=$(pwd)/zulu11.39.15-ca-jre11.0.7-macosx_x64
 export PATH=$JAVA_HOME/bin:$PATH
-neo4j-enterprise-4.0.4/bin/neo4j-admin set-initial-password trinity
+neo4j-enterprise-4.0.6/bin/neo4j-admin set-initial-password trinity
--- scripts/fourohandbeyond/mac/unpack.sh
+++ scripts/fourohandbeyond/mac/unpack.sh
@@ -1,4 +1,8 @@
-tar -xzf install/zulu11.39.15-ca-jdk11.0.7-macosx_x64.tar.gz
-tar -xzf install/neo4j-enterprise-4.0.4-unix.tar.gz
-mv install/apoc-4.0.0.12-all.jar neo4j-enterprise-4.0.4/plugins
-mv install/apoc-nlp-dependencies-4.0.0.12.jar neo4j-enterprise-4.0.4/plugins
+unzip ./install/zulu11.39.15-ca-jre11.0.7-macosx_x64.zip
+tar -xzf install/neo4j-enterprise-4.0.6-unix.tar.gz
+unzip ./install/neo4j-graph-data-science-1.2.2-standalone.zip -d ./install
+
+mv install/apoc-4.0.0.15-all.jar neo4j-enterprise-4.0.6/plugins
+mv install/apoc-nlp-dependencies-4.0.0.15.jar neo4j-enterprise-4.0.6/plugins
+mv install/apoc-mongodb-dependencies-4.0.0.15.jar neo4j-enterprise-4.0.6/plugins
+mv install/neo4j-graph-data-science-1.2.2-standalone.jar neo4j-enterprise-4.0.6/plugins
