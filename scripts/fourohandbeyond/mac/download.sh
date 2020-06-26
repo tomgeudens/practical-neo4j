319ecf4709ad3ea92b0e07c3279eaa9b7dd60956
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-06-26 08:47:14 +0200

    update mac scripts 4.1.0

--- scripts/fourohandbeyond/mac/download.sh
+++ scripts/fourohandbeyond/mac/download.sh
@@ -1,7 +1,7 @@
 mkdir install
 curl -L -o "install/zulu11.39.15-ca-jre11.0.7-macosx_x64.zip" https://cdn.azul.com/zulu/bin/zulu11.39.15-ca-jre11.0.7-macosx_x64.zip
-curl -L -o "install/neo4j-enterprise-4.0.6-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-4.0.6-unix.tar.gz
-curl -L -o "install/apoc-4.0.0.15-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.15/apoc-4.0.0.15-all.jar
-curl -L -o "install/apoc-nlp-dependencies-4.0.0.15.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.15/apoc-nlp-dependencies-4.0.0.15.jar
-curl -L -o "install/apoc-mongodb-dependencies-4.0.0.15.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.15/apoc-mongodb-dependencies-4.0.0.15.jar
+curl -L -o "install/neo4j-enterprise-4.1.0-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-4.1.0-unix.tar.gz
+curl -L -o "install/apoc-4.1.0.0-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-4.1.0.0-all.jar
+curl -L -o "install/apoc-nlp-dependencies-4.1.0.0.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-nlp-dependencies-4.1.0.0.jar
+curl -L -o "install/apoc-mongodb-dependencies-4.1.0.0.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-mongodb-dependencies-4.1.0.0.jar
 curl -L -o "install/neo4j-graph-data-science-1.2.2-standalone.zip" https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/graph-data-science/neo4j-graph-data-science-1.2.2-standalone.zip
