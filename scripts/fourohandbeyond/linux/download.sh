639e420b8a4fb9d5886c0c3392228dde012c48fb
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-06-26 07:57:31 +0200

    update linux scripts 4.1.0

--- scripts/fourohandbeyond/linux/download.sh
+++ scripts/fourohandbeyond/linux/download.sh
@@ -1,7 +1,7 @@
 mkdir install
 wget -O "install/zulu11.39.15-ca-jre11.0.7-linux_x64.tar.gz" https://cdn.azul.com/zulu/bin/zulu11.39.15-ca-jre11.0.7-linux_x64.tar.gz
-wget -O "install/neo4j-enterprise-4.0.6-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-4.0.6-unix.tar.gz
-wget -O "install/apoc-4.0.0.15-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.15/apoc-4.0.0.15-all.jar
-wget -O "install/apoc-nlp-dependencies-4.0.0.15.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.15/apoc-nlp-dependencies-4.0.0.15.jar
-wget -O "install/apoc-mongodb-dependencies-4.0.0.15.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.15/apoc-mongodb-dependencies-4.0.0.15.jar
+wget -O "install/neo4j-enterprise-4.1.0-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-4.1.0-unix.tar.gz
+wget -O "install/apoc-4.1.0.0-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-4.1.0.0-all.jar
+wget -O "install/apoc-nlp-dependencies-4.1.0.0.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-nlp-dependencies-4.1.0.0.jar
+wget -O "install/apoc-mongodb-dependencies-4.1.0.0.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-mongodb-dependencies-4.1.0.0.jar
 wget -O "install/neo4j-graph-data-science-1.2.2-standalone.zip" https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/graph-data-science/neo4j-graph-data-science-1.2.2-standalone.zip
