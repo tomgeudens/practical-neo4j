74fede88d68cd5e9b3459a8aa19dd1ddea51cf24
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-07-14 10:40:34 +0200

    updated linux for 3.5.20

--- scripts/threefive/linux/settings.sh
+++ scripts/threefive/linux/settings.sh
@@ -1,14 +1,18 @@
+export neo4jVersion="3.5.20"
+export apocVersion="3.5.0.12"
+export gdsVersion="1.1.3"
+
 # changes to neo4j.conf file
-echo "metrics.enabled=false" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.security.procedures.unrestricted=apoc.*,gds.*" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.connectors.default_listen_address=0.0.0.0" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.memory.heap.initial_size=1024m" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.memory.heap.max_size=1024m" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.udc.enabled=false" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
-echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-3.5.19/conf/neo4j.conf
+echo "metrics.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.security.procedures.unrestricted=apoc.*,gds.*" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.connectors.default_listen_address=0.0.0.0" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.heap.initial_size=1024m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.heap.max_size=1024m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.udc.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 
 # setting the initial password
 export JAVA_HOME=$(pwd)/zulu8.46.0.19-ca-jre8.0.252-linux_x64
 export PATH=$JAVA_HOME/bin:$PATH
-neo4j-enterprise-3.5.19/bin/neo4j-admin set-initial-password trinity
+neo4j-enterprise-${neo4jVersion}/bin/neo4j-admin set-initial-password trinity
--- scripts/threefive/linux/unpack.sh
+++ scripts/threefive/linux/unpack.sh
@@ -1,7 +1,11 @@
-tar -xzf install/zulu8.46.0.19-ca-jre8.0.252-linux_x64.tar.gz
-tar -xzf install/neo4j-enterprise-3.5.19-unix.tar.gz
-unzip -q install/neo4j-graph-data-science-1.1.3-standalone.zip -d install
+export neo4jVersion="3.5.20"
+export apocVersion="3.5.0.12"
+export gdsVersion="1.1.3"
 
-mv install/apoc-3.5.0.12-all.jar neo4j-enterprise-3.5.19/plugins
-mv install/apoc-mongodb-dependencies-3.5.0.12.jar neo4j-enterprise-3.5.19/plugins
-mv install/neo4j-graph-data-science-1.1.3-standalone.jar neo4j-enterprise-3.5.19/plugins
+tar -xzf install/zulu8.46.0.19-ca-jre8.0.252-linux_x64.tar.gz
+tar -xzf install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz
+unzip -q install/neo4j-graph-data-science-${gdsVersion}-standalone.zip -d install
+
+mv install/apoc-${apocVersion}-all.jar neo4j-enterprise-${neo4jVersion}/plugins
+mv install/apoc-mongodb-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
+mv install/neo4j-graph-data-science-${gdsVersion}-standalone.jar neo4j-enterprise-${neo4jVersion}/plugins
