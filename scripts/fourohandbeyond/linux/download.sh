9abbcc09d828d66d8dbf695de177bed21cef481a
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-07-15 13:20:33 +0200

    updated linux scripts for 4.1.1

--- scripts/fourohandbeyond/linux/download.sh
+++ scripts/fourohandbeyond/linux/download.sh
@@ -1,7 +1,12 @@
 mkdir install
+
+export neo4jVersion="4.1.1"
+export apocVersion="4.1.0.0"
+export gdsVersion="1.3.0"
+
 wget -O "install/zulu11.39.15-ca-jre11.0.7-linux_x64.tar.gz" https://cdn.azul.com/zulu/bin/zulu11.39.15-ca-jre11.0.7-linux_x64.tar.gz
-wget -O "install/neo4j-enterprise-4.1.0-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-4.1.0-unix.tar.gz
-wget -O "install/apoc-4.1.0.0-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-4.1.0.0-all.jar
-wget -O "install/apoc-nlp-dependencies-4.1.0.0.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-nlp-dependencies-4.1.0.0.jar
-wget -O "install/apoc-mongodb-dependencies-4.1.0.0.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.0/apoc-mongodb-dependencies-4.1.0.0.jar
-wget -O "install/neo4j-graph-data-science-1.3.0-standalone.zip" https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/graph-data-science/neo4j-graph-data-science-1.3.0-standalone.zip
+wget -O "install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-${neo4jVersion}-unix.tar.gz
+wget -O "install/apoc-${apocVersion}-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-${apocVersion}-all.jar
+wget -O "install/apoc-nlp-dependencies-${apocVersion}.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-nlp-dependencies-${apocVersion}.jar
+wget -O "install/apoc-mongodb-dependencies-${apocVersion}.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-mongodb-dependencies-${apocVersion}.jar
+wget -O "install/neo4j-graph-data-science-${gdsVersion}-standalone.zip"  https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/graph-data-science/neo4j-graph-data-science-${gdsVersion}-standalone.zip
--- scripts/fourohandbeyond/linux/settings.sh
+++ scripts/fourohandbeyond/linux/settings.sh
@@ -1,18 +1,22 @@
+export neo4jVersion="4.1.1"
+export apocVersion="4.1.0.0"
+export gdsVersion="1.3.0"
+
 # changes to neo4j.conf file
-echo "metrics.enabled=false" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.security.procedures.unrestricted=apoc.*,gds.*" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.default_listen_address=0.0.0.0" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.logs.query.enabled=off" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.memory.heap.initial_size=2048m" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.memory.heap.max_size=2048m" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
-echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-4.1.0/conf/neo4j.conf
+echo "metrics.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.security.procedures.unrestricted=apoc.*,gds.*" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.default_listen_address=0.0.0.0" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.logs.query.enabled=off" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.heap.initial_size=2048m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.heap.max_size=2048m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 
 # create apoc.conf file
-echo "apoc.export.file.enabled=true" > neo4j-enterprise-4.1.0/conf/apoc.conf
-echo "apoc.import.file.enabled=true" >> neo4j-enterprise-4.1.0/conf/apoc.conf
+echo "apoc.export.file.enabled=true" > neo4j-enterprise-${neo4jVersion}/conf/apoc.conf
+echo "apoc.import.file.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/apoc.conf
 
 # setting the initial password
 export JAVA_HOME=$(pwd)/zulu11.39.15-ca-jre11.0.7-linux_x64
 export PATH=$JAVA_HOME/bin:$PATH
-neo4j-enterprise-4.1.0/bin/neo4j-admin set-initial-password trinity
+neo4j-enterprise-${neo4jVersion}/bin/neo4j-admin set-initial-password trinity
