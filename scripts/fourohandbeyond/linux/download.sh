6590cb76a191adbc3b2351ee817154be53cd8b51
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-07-21 08:17:49 +0200

    update for latest Zulu java jre release - fix download.sh

--- scripts/fourohandbeyond/linux/download.sh
+++ scripts/fourohandbeyond/linux/download.sh
@@ -6,7 +6,7 @@ export gdsVersion="1.3.0"
 export zuluVersion="11.41.23"
 export jreVersion="11.0.8"
 
-wget -O "install/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64.tar.gz" https://cdn.azul.com/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64.tar.gz
+wget -O "install/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64.tar.gz" https://cdn.azul.com/zulu/bin/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64.tar.gz
 wget -O "install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-${neo4jVersion}-unix.tar.gz
 wget -O "install/apoc-${apocVersion}-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-${apocVersion}-all.jar
 wget -O "install/apoc-nlp-dependencies-${apocVersion}.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-nlp-dependencies-${apocVersion}.jar
