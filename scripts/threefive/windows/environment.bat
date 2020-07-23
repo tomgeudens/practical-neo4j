baf189af71e5b9afd074359fbed5b1e0690744e3
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-07-23 10:57:48 +0200

    more efficient windows scripts - update java 8

--- scripts/threefive/windows/environment.bat
+++ scripts/threefive/windows/environment.bat
@@ -1,8 +1,8 @@
 set apocVersion=3.5.0.13
 set neo4jVersion=3.5.20
 set gdsVersion=1.1.3
-set zuluVersion=8.46.0.19
-set jreVersion=8.0.252
+set zuluVersion=8.48.0.51
+set jreVersion=8.0.262
 
 set JAVA_HOME=%CD%\zulu%zuluVersion%-ca-jre%jreVersion%-win_x64
 set PATH=%JAVA_HOME%\bin;%PATH%
