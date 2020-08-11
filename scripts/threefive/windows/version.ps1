cf92ef2d5348416105fc7d48566f688baa7b34d1
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-08-11 14:53:37 +0200

    Update for GDS release 1.1.4 windows

--- scripts/threefive/windows/version.ps1
+++ scripts/threefive/windows/version.ps1
@@ -2,7 +2,7 @@ Write-Host "Setting the versions ..."
 
 $apocVersion = "3.5.0.13"
 $neo4jVersion = "3.5.20"
-$gdsVersion = "1.1.3"
+$gdsVersion = "1.1.4"
 $zuluVersion = "8.48.0.51"
 $jreVersion = "8.0.262"
 
