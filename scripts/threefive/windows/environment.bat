68d9b445429e34ab1baf3d4f4c335488a0a7d677
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-08-24 09:07:35 +0200

    Update retro to 3.5.21 and latest Zulu patch

--- scripts/threefive/windows/environment.bat
+++ scripts/threefive/windows/environment.bat
@@ -1,8 +1,8 @@
 set apocVersion=3.5.0.14
-set neo4jVersion=3.5.20
+set neo4jVersion=3.5.21
 set gdsVersion=1.1.4
-set zuluVersion=8.48.0.51
-set jreVersion=8.0.262
+set zuluVersion=8.48.0.53
+set jreVersion=8.0.265
 
 set JAVA_HOME=%CD%\zulu%zuluVersion%-ca-jre%jreVersion%-win_x64
 set PATH=%JAVA_HOME%\bin;%PATH%
--- scripts/threefive/windows/version.ps1
+++ scripts/threefive/windows/version.ps1
@@ -1,9 +1,9 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "3.5.0.14"
-$neo4jVersion = "3.5.20"
+$neo4jVersion = "3.5.21"
 $gdsVersion = "1.1.4"
-$zuluVersion = "8.48.0.51"
-$jreVersion = "8.0.262"
+$zuluVersion = "8.48.0.53"
+$jreVersion = "8.0.265"
 
 Write-Host " Done!" -ForegroundColor Green
