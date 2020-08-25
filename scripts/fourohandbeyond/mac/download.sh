504547e167b62cd2e152d31c9321fa102e6df732
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-08-25 11:25:24 +0200

    MAC java download is a zip, not a tar.gz

--- scripts/fourohandbeyond/mac/download.sh
+++ scripts/fourohandbeyond/mac/download.sh
@@ -2,7 +2,7 @@ mkdir install
 
 . ./scripts/version.sh
 
-curl -L -o "install/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64.tar.gz" https://cdn.azul.com/zulu/bin/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64.tar.gz
+curl -L -o "install/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64.zip" https://cdn.azul.com/zulu/bin/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64.zip
 curl -L -o "install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz" https://neo4j.com/artifact.php?name=neo4j-enterprise-${neo4jVersion}-unix.tar.gz
 curl -L -o "install/apoc-${apocVersion}-all.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-${apocVersion}-all.jar
 curl -L -o "install/apoc-nlp-dependencies-${apocVersion}.jar" https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-nlp-dependencies-${apocVersion}.jar
--- scripts/fourohandbeyond/mac/unpack.sh
+++ scripts/fourohandbeyond/mac/unpack.sh
@@ -1,6 +1,6 @@
 . ./scripts/version.sh
 
-tar -xzf install/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64.tar.gz
+unzip -q install/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64.zip
 tar -xzf install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz
 unzip -q install/neo4j-graph-data-science-${gdsVersion}-standalone.zip -d install
 
