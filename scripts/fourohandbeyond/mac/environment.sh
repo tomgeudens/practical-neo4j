156b6ecdb467ca601a635627c2db223b5d80dbc1
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-11-01 11:04:56 +0100

    extra enviroment parameter

--- scripts/fourohandbeyond/mac/environment.sh
+++ scripts/fourohandbeyond/mac/environment.sh
@@ -1,4 +1,5 @@
 . ./scripts/version.sh
 
+export NEO4J_HOME=$(pwd)/neo4j-enterprise-${neo4jVersion}
 export JAVA_HOME=$(pwd)/zulu${zuluVersion}-ca-jre${jreVersion}-macosx_x64
 export PATH=$JAVA_HOME/bin:$PATH
--- scripts/fourohandbeyond/windows/environment.bat
+++ scripts/fourohandbeyond/windows/environment.bat
@@ -4,5 +4,6 @@ set gdsVersion=1.3.4
 set zuluVersion=11.41.23
 set jreVersion=11.0.8
 
+set NEO4J_HOME=%CD%\neo4j-enterprise-%neo4jVersion%
 set JAVA_HOME=%CD%\zulu%zuluVersion%-ca-jre%jreVersion%-win_x64
 set PATH=%JAVA_HOME%\bin;%PATH%
