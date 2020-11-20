fd650f98f18ac3ea281748e08a3d7e0a531765a8
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-11-20 10:28:45 +0100

    update windows environment

--- scripts/fourohandbeyond/windows/environment.bat
+++ scripts/fourohandbeyond/windows/environment.bat
@@ -1,8 +1,8 @@
-set apocVersion=4.1.0.2
-set neo4jVersion=4.1.3
-set gdsVersion=1.3.4
-set zuluVersion=11.41.23
-set jreVersion=11.0.8
+set apocVersion=4.1.0.4
+set neo4jVersion=4.1.4
+set gdsVersion=1.4.0
+set zuluVersion=11.43.55
+set jreVersion=11.0.9.1
 
 set NEO4J_HOME=%CD%\neo4j-enterprise-%neo4jVersion%
 set JAVA_HOME=%CD%\zulu%zuluVersion%-ca-jre%jreVersion%-win_x64
