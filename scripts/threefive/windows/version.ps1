a0283bff2940227cb43045d03bc60e9dfcf1aba7
Tom Geudens <tomgeudens@users.noreply.github.com>
2020-12-01 17:54:26 +0100

    3.5.25

--- scripts/threefive/windows/version.ps1
+++ scripts/threefive/windows/version.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "3.5.0.15"
-$neo4jVersion = "3.5.24"
+$neo4jVersion = "3.5.25"
 $gdsVersion = "1.1.6"
 $zuluVersion = "8.50.0.51"
 $jreVersion = "8.0.275"
