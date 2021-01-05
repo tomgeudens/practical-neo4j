47ccb7e15547e6fb90450170b53dec06dcb8a115
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-01-05 06:51:24 +0100

    fixed setting

--- scripts/fourohandbeyond/linux/settings.sh
+++ scripts/fourohandbeyond/linux/settings.sh
@@ -1,12 +1,16 @@
 . ./scripts/version.sh
 
 # changes to neo4j.conf file
-echo "metrics.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "metrics.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "metrics.csv.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "metrics.prometheus.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "metrics.graphite.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "metrics.jmx.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 echo "dbms.security.procedures.unrestricted=apoc.*,gds.*" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 echo "dbms.default_listen_address=0.0.0.0" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 echo "dbms.logs.query.enabled=off" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
-echo "dbms.memory.heap.initial_size=2048m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
-echo "dbms.memory.heap.max_size=2048m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.heap.initial_size=2g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.heap.max_size=2g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 echo "dbms.memory.pagecache.size=1g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 echo "dbms.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
 
