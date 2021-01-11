89db809fa8489e7410d1bce38c9f329f9fc06f3c
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-01-11 08:34:12 +0100

    Version 4.2.2 Linux

--- scripts/fourohandbeyond/linux/unpack.sh
+++ scripts/fourohandbeyond/linux/unpack.sh
@@ -3,8 +3,3 @@
 tar -xzf install/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64.tar.gz
 tar -xzf install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz
 unzip -q install/neo4j-graph-data-science-${gdsVersion}-standalone.zip -d install
-
-mv install/apoc-${apocVersion}-all.jar neo4j-enterprise-${neo4jVersion}/plugins
-mv install/apoc-mongodb-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
-mv install/apoc-nlp-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
-mv install/neo4j-graph-data-science-${gdsVersion}-standalone.jar neo4j-enterprise-${neo4jVersion}/plugins
--- scripts/fourohandbeyond/linux/version.sh
+++ scripts/fourohandbeyond/linux/version.sh
@@ -1,4 +1,4 @@
-export neo4jVersion="4.2.1"
+export neo4jVersion="4.2.2"
 export apocVersion="4.2.0.0"
 export gdsVersion="1.4.1"
 export zuluVersion="11.43.55"
