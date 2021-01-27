791cabd485bf5a981b7bada4464009970ac15f5a
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-01-27 06:22:46 +0100

    neo4j 4.2.3, apoc 4.2.0.1, zulu 11.45.27

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,9 +1,9 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$apocVersion = "4.2.0.0"
-$neo4jVersion = "4.2.2"
+$apocVersion = "4.2.0.1"
+$neo4jVersion = "4.2.3"
 $gdsVersion = "1.4.1"
-$zuluVersion = "11.43.55"
-$jreVersion = "11.0.9.1"
+$zuluVersion = "11.45.27"
+$jreVersion = "11.0.10"
 
 Write-Host " Done!" -ForegroundColor Green
