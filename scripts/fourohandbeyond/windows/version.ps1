bf61efea80f9d47f23f01aa080480bbf82283451
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-02-19 07:54:12 +0100

    new GDS version 1.5.0

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -2,7 +2,7 @@ Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "4.2.0.1"
 $neo4jVersion = "4.2.3"
-$gdsVersion = "1.4.1"
+$gdsVersion = "1.5.0"
 $zuluVersion = "11.45.27"
 $jreVersion = "11.0.10"
 
