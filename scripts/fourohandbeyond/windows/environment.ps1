ac90268a43abe3c298d0a615d1e245cecc1836cb
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-03-15 08:10:31 +0100

    conditional remove

--- scripts/fourohandbeyond/windows/environment.ps1
+++ scripts/fourohandbeyond/windows/environment.ps1
@@ -5,6 +5,8 @@
 $javaJRELocation = Join-Path (Get-Location).Path "zulu$($zuluVersion)-ca-jre$($jreVersion)-win_x64"
 
 # Environment
-Remove-Item env:_JAVA_OPTIONS -Force
+if (Test-Path env:_JAVA_OPTIONS) {
+  Remove-Item env:_JAVA_OPTIONS
+}
 $env:JAVA_HOME = $javaJRELocation
 $env:PATH = "$($javaJRELocation)\bin;" + $env:PATH
--- scripts/fourohandbeyond/windows/start.ps1
+++ scripts/fourohandbeyond/windows/start.ps1
@@ -9,7 +9,9 @@ $neo4jLocation = Join-Path (Get-Location).Path "neo4j-enterprise-$($neo4jVersion
 $javaJRELocation = Join-Path (Get-Location).Path "zulu$($zuluVersion)-ca-jre$($jreVersion)-win_x64"
 
 Write-Host "Setting Java Environment for this session ... " -NoNewline
-Remove-Item env:_JAVA_OPTIONS -Force
+if (Test-Path env:_JAVA_OPTIONS) {
+  Remove-Item env:_JAVA_OPTIONS
+}
 $env:JAVA_HOME = $javaJRELocation
 $env:PATH = "$($javaJRELocation)\bin;" + $env:PATH
 Write-Host "Done!" -ForegroundColor Green
