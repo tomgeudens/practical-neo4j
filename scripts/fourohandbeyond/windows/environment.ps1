a7a871d036901dc8f0552c6a0ec05b80fded6f05
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-03-15 08:00:41 +0100

    Remove _JAVA_OPTIONS from the equation

--- scripts/fourohandbeyond/windows/environment.ps1
+++ scripts/fourohandbeyond/windows/environment.ps1
@@ -5,5 +5,6 @@
 $javaJRELocation = Join-Path (Get-Location).Path "zulu$($zuluVersion)-ca-jre$($jreVersion)-win_x64"
 
 # Environment
+Remove-Item env:_JAVA_OPTIONS
 $env:JAVA_HOME = $javaJRELocation
 $env:PATH = "$($javaJRELocation)\bin;" + $env:PATH
