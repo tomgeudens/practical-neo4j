dff44d11917ad19d0a692771cd6499e5c94eae66
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-04-19 08:13:09 +0200

    update windows 4.2.5

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "4.2.0.2"
-$neo4jVersion = "4.2.4"
+$neo4jVersion = "4.2.5"
 $gdsVersion = "1.5.1"
 $zuluVersion = "11.45.27"
 $jreVersion = "11.0.10"
