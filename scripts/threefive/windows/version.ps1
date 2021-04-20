3477fcd33ad07ad9383702aed84e92e7c9a862e2
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-04-20 11:49:04 +0200

    update windows 3.5.27, jre 8.0.282

--- scripts/threefive/windows/version.ps1
+++ scripts/threefive/windows/version.ps1
@@ -1,9 +1,9 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "3.5.0.15"
-$neo4jVersion = "3.5.25"
+$neo4jVersion = "3.5.27"
 $gdsVersion = "1.1.6"
-$zuluVersion = "8.50.0.51"
-$jreVersion = "8.0.275"
+$zuluVersion = "8.52.0.23"
+$jreVersion = "8.0.282"
 
 Write-Host " Done!" -ForegroundColor Green
