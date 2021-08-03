6257bf72efa202df1954b8693921964234241837
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-08-03 05:10:00 +0200

    GDS 1.6.4, Zulu Java 11.0.12

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -2,8 +2,8 @@ Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "4.3.0.0"
 $neo4jVersion = "4.3.2"
-$gdsVersion = "1.6.2"
-$zuluVersion = "11.48.21"
-$jreVersion = "11.0.11"
+$gdsVersion = "1.6.4"
+$zuluVersion = "11.50.19"
+$jreVersion = "11.0.12"
 
 Write-Host " Done!" -ForegroundColor Green
