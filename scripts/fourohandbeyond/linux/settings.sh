2ee5b528cc853db54bc0e6d560197c2c0e73536a
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-08-03 06:11:45 +0200

    safer variable use in settings

--- scripts/fourohandbeyond/linux/settings.sh
+++ scripts/fourohandbeyond/linux/settings.sh
@@ -21,6 +21,6 @@ echo "apoc.import.file.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/ap
 echo "apoc.import.file.use_neo4j_config=true" >> neo4j-enterprise-${neo4jVersion}/conf/apoc.conf
 
 # setting the initial password
-export JAVA_HOME=$(pwd)/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64
-export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME="$(pwd)/zulu${zuluVersion}-ca-jre${jreVersion}-linux_x64"
+export PATH="$JAVA_HOME/bin:$PATH"
 neo4j-enterprise-${neo4jVersion}/bin/neo4j-admin set-initial-password trinity
