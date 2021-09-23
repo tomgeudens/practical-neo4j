3b85119533d947148b541986517ed5685566bcd6
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-09-23 12:48:04 +0200

    Linux script changes 4.3.4

--- scripts/fourohandbeyond/linux/move.sh
+++ scripts/fourohandbeyond/linux/move.sh
@@ -5,6 +5,7 @@ mv install/apoc-couchbase-dependencies-${apocVersion}.jar neo4j-enterprise-${neo
 mv install/apoc-email-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
 mv install/apoc-mongodb-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
 mv install/apoc-nlp-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
+mv install/apoc-redis-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
 mv install/apoc-xls-dependencies-${apocVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
 #mv install/license-dependency.json neo4j-enterprise-${neo4jVersion}/plugins
 mv install/neo4j-graph-data-science-${gdsVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
