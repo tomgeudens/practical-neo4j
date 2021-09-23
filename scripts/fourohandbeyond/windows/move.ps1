c1b8ae7499ebd4f269c743dbb8e8d0ba71b10c6e
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-09-23 09:24:40 +0200

    APOC 4.3.0.1 has Redis plugin

--- scripts/fourohandbeyond/windows/move.ps1
+++ scripts/fourohandbeyond/windows/move.ps1
@@ -21,7 +21,7 @@ $apocJar = "apoc-$($apocVersion)-all.jar"
 $apocLicense = "license-dependency.json"
 
 # APOC Dependency list
-$dependencies = "couchbase","email","mongodb","nlp","xls"
+$dependencies = "couchbase","email","mongodb","nlp","redis","xls"
 
 # Plugins
 $plugins = @{}
@@ -42,7 +42,7 @@ Foreach ($dependency in $dependencies) {
 $locations.Add('apoclicense', (Get-Location).Path + "\neo4j-enterprise-$($neo4jVersion)\plugins")
 
 # Catalog of products
-$catalog = "gds","apoc","apoccouchbase","apocemail","apocmongodb","apocnlp","apocxls"
+$catalog = "gds","apoc","apoccouchbase","apocemail","apocmongodb","apocnlp","apocredis","apocxls"
 
 # Actual move
 Foreach ($item in $catalog) {
