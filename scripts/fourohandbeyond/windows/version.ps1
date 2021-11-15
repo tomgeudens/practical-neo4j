bcfb10398030974aabe418a4d16b91f205398c86
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-11-15 09:43:56 +0100

    Neo4j 4.3.7, APOC 4.3.0.4

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$apocVersion = "4.3.0.3"
-$neo4jVersion = "4.3.6"
+$apocVersion = "4.3.0.4"
+$neo4jVersion = "4.3.7"
 $gdsVersion = "1.7.2"
 $zuluVersion = "11.52.13"
 $jreVersion = "11.0.13"
