c8cfd7a403acf41dbf770ed6ab4800c38e0e952a
Tom Geudens <tomgeudens@users.noreply.github.com>
2021-11-30 06:55:16 +0100

    Neo4j 3.5.30, APOC 3.5.0.15

--- scripts/threefive/windows/version.ps1
+++ scripts/threefive/windows/version.ps1
@@ -1,9 +1,9 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $apocVersion = "3.5.0.15"
-$neo4jVersion = "3.5.27"
-$gdsVersion = "1.1.6"
-$zuluVersion = "8.52.0.23"
-$jreVersion = "8.0.282"
+$neo4jVersion = "3.5.30"
+$gdsVersion = "1.1.7"
+$zuluVersion = "8.58.0.13"
+$jreVersion = "8.0.312"
 
 Write-Host " Done!" -ForegroundColor Green
