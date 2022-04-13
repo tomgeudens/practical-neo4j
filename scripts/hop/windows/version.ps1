d0fdec3a78b9317e7c6f25c348bb8636f9e0370e
Tom Geudens <tomgeudens@users.noreply.github.com>
2022-04-13 07:36:13 +0200

    fix version.ps1 hop

--- scripts/hop/windows/version.ps1
+++ scripts/hop/windows/version.ps1
@@ -7,5 +7,3 @@ $temurinMajorVersion = "8"
 $temurinType = "jre"
 
 Write-Host " Done!" -ForegroundColor Green
-https://www.apache.org/dyn/closer.cgi?filename=hop/1.2.0/apache-hop-client-1.2.0.zip&action=download
-https://dlcdn.apache.org/hop/1.2.0/apache-hop-client-1.2.0.zip
