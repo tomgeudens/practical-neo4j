12019ddce131edf6d47a2f28ac2eaba3e91d099f
Tom Geudens <tomgeudens@users.noreply.github.com>
2022-05-13 10:10:07 +0200

    APOC 4.4.0.5, GDS 2.0.4

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,8 +1,8 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$apocVersion = "4.4.0.3"
+$apocVersion = "4.4.0.5"
 $neo4jVersion = "4.4.6"
-$gdsVersion = "2.0.2"
+$gdsVersion = "2.0.4"
 $zuluVersion = "11.56.19"
 $jreVersion = "11.0.15"
 
