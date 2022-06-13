41f664c4f3bd6b8a6039b431d107230e6737d60b
Tom Geudens <tomgeudens@users.noreply.github.com>
2022-06-13 11:12:04 +0200

    fix in move.sh

--- scripts/fourohandbeyond/linux/move.sh
+++ scripts/fourohandbeyond/linux/move.sh
@@ -1,4 +1,4 @@
 . ./scripts/version.sh
 
-mv neo4j-enterprise-${neo4jVersion}/lab/apoc*core.jar neo4j-enterprise-${neo4jVersion}/plugins
+mv neo4j-enterprise-${neo4jVersion}/labs/apoc*core.jar neo4j-enterprise-${neo4jVersion}/plugins
 mv install/neo4j-graph-data-science-${gdsVersion}.jar neo4j-enterprise-${neo4jVersion}/plugins
