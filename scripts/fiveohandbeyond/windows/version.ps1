997ef915b59c5340a6aa625f8ada9b62f7f258e1
Tom Geudens <tomgeudens@users.noreply.github.com>
2022-12-13 11:08:34 +0100

    GDS 2.2.5

--- scripts/fiveohandbeyond/windows/version.ps1
+++ scripts/fiveohandbeyond/windows/version.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $neo4jVersion = "5.2.0"
-$gdsVersion = "2.2.4"
+$gdsVersion = "2.2."
 $javaVersion="17"
 $temurinURLVersion="17.0.5%2B8"
 $temurinFileVersion="17.0.5_8"
