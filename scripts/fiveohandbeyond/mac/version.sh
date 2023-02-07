6af72a3ab83b80446614170051241205580e7c56
Tom Geudens <tomgeudens@users.noreply.github.com>
2023-02-07 06:50:19 +0100

    Neo4j 5.4.0, GDS 2.3.0, Java 17.0.6

--- scripts/fiveohandbeyond/mac/version.sh
+++ scripts/fiveohandbeyond/mac/version.sh
@@ -1,5 +1,5 @@
 export neo4jVersion="5.4.0"
-export gdsVersion="2.2.7"
+export gdsVersion="2.3.0"
 export javaVersion="17"
 export temurinURLVersion="17.0.6%2B10"
 export temurinFileVersion="17.0.6_10"
--- scripts/fiveohandbeyond/windows/version.ps1
+++ scripts/fiveohandbeyond/windows/version.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
 $neo4jVersion = "5.4.0"
-$gdsVersion = "2.2.7"
+$gdsVersion = "2.3.0"
 $javaVersion="17"
 $temurinURLVersion="17.0.6%2B10"
 $temurinFileVersion="17.0.6_10"
