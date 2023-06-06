57c056aab64eb66e4b31cbccbe244bb2643c5819
Tom Geudens <tomgeudens@users.noreply.github.com>
2023-06-06 16:46:34 +0200

    GDS 2.3.8

--- scripts/fiveohandbeyond/linux/version.sh
+++ scripts/fiveohandbeyond/linux/version.sh
@@ -1,5 +1,5 @@
 export neo4jVersion="5.8.0"
-export gdsVersion="2.3.6"
+export gdsVersion="2.3.8"
 export javaVersion="17"
 export temurinURLVersion="17.0.7%2B7"
 export temurinFileVersion="17.0.7_7"
