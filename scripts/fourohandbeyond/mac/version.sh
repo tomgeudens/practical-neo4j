b7f12654ae0995336c4d6321e6ee11856c285353
Tom Geudens <tomgeudens@users.noreply.github.com>
2023-06-30 08:03:25 +0200

    Neo4j 4.4.22, GDS 2.4.1

--- scripts/fourohandbeyond/mac/version.sh
+++ scripts/fourohandbeyond/mac/version.sh
@@ -1,4 +1,4 @@
-export neo4jVersion="4.4.21"
-export gdsVersion="2.4.0"
+export neo4jVersion="4.4.22"
+export gdsVersion="2.4.1"
 export zuluVersion="11.64.19"
 export jreVersion="11.0.19"
--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "4.4.21"
-$gdsVersion = "2.4.0"
+$neo4jVersion = "4.4.22"
+$gdsVersion = "2.4.1"
 $zuluVersion = "11.64.19"
 $jreVersion = "11.0.19"
 
