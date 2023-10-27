5856d4e979e95cf82c34ac29896b8d4658b443da
Tom Geudens <tomgeudens@users.noreply.github.com>
2023-10-27 11:34:16 +0200

    Windows back to Java 17.0.8.1

--- scripts/fiveohandbeyond/windows/version.ps1
+++ scripts/fiveohandbeyond/windows/version.ps1
@@ -3,8 +3,8 @@ Write-Host "Setting the versions ..." -NoNewline;
 $neo4jVersion = "5.13.0"
 $gdsVersion = "2.5.2"
 $javaVersion="17"
-$temurinURLVersion="17.0.9%2B9"
-$temurinFileVersion="17.0.9_9"
-$temurinHomeVersion="17.0.9+9"
+$temurinURLVersion="17.0.8.1%2B1"
+$temurinFileVersion="17.0.8.1_1"
+$temurinHomeVersion="17.0.8.1+1"
 
 Write-Host " Done!" -ForegroundColor Green
