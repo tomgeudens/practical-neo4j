626117a444fd29e3a79d31a7f1bffefcfd20ba78
Tom Geudens <tomgeudens@users.noreply.github.com>
2023-12-01 09:32:57 +0100

    Fix Java entries Windows

--- scripts/fiveohandbeyond/windows/version.ps1
+++ scripts/fiveohandbeyond/windows/version.ps1
@@ -4,7 +4,7 @@ $neo4jVersion = "5.14.0"
 $gdsVersion = "2.5.5"
 $javaVersion="17"
 $temurinURLVersion="17.0.9%2B9.1"
-$temurinFileVersion="17.0.9_9.1"
+$temurinFileVersion="17.0.9_9"
 $temurinHomeVersion="17.0.9+9.1"
 
 Write-Host " Done!" -ForegroundColor Green
