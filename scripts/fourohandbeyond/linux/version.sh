19a21d06468975d3704b79ea1c5305dd6d594555
Tom Geudens <tomgeudens@users.noreply.github.com>
2024-02-13 20:21:29 +0100

    Neo4j 4.4.30, GDS 2.6.0, Java 11.0.22

--- scripts/fourohandbeyond/linux/version.sh
+++ scripts/fourohandbeyond/linux/version.sh
@@ -1,4 +1,4 @@
-export neo4jVersion="4.4.28"
-export gdsVersion="2.5.4"
-export zuluVersion="11.68.17"
-export jreVersion="11.0.21"
+export neo4jVersion="4.4.30"
+export gdsVersion="2.6.0"
+export zuluVersion="11.70.15"
+export jreVersion="11.0.22"
--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,8 +1,8 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "4.4.28"
-$gdsVersion = "2.5.4"
-$zuluVersion = "11.68.17"
-$jreVersion = "11.0.21"
+$neo4jVersion = "4.4.30"
+$gdsVersion = "2.6.0"
+$zuluVersion = "11.70.15"
+$jreVersion = "11.0.22"
 
 Write-Host " Done!" -ForegroundColor Green
