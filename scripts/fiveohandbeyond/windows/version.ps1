588339aadd1590f7469bfb8d56a6abca56cdebf5
Tom Geudens <tomgeudens@users.noreply.github.com>
2024-07-18 13:53:03 +0200

    Neo4j 5.21.2, Java 17.0.12

--- scripts/fiveohandbeyond/windows/version.ps1
+++ scripts/fiveohandbeyond/windows/version.ps1
@@ -1,10 +1,10 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "5.21.0"
+$neo4jVersion = "5.21.2"
 $gdsVersion = "2.7.0"
 $javaVersion="17"
-$temurinURLVersion="17.0.11%2B9"
-$temurinFileVersion="17.0.11_9"
-$temurinHomeVersion="17.0.11+9"
+$temurinURLVersion="17.0.12%2B7"
+$temurinFileVersion="17.0.12_7"
+$temurinHomeVersion="17.0.12+7"
 
 Write-Host " Done!" -ForegroundColor Green
