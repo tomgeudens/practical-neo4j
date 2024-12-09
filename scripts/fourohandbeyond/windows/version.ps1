1715cc11219caef34822efd570c2b70b62fb7704
Tom Geudens <tomgeudens@users.noreply.github.com>
2024-12-09 07:55:51 +0100

    Neo4j 4.4.39, Java 11.0.25

--- scripts/fourohandbeyond/windows/version.ps1
+++ scripts/fourohandbeyond/windows/version.ps1
@@ -1,8 +1,8 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "4.4.37"
+$neo4jVersion = "4.4.39"
 $gdsVersion = "2.6.8"
-$zuluVersion = "11.74.15"
-$jreVersion = "11.0.24"
+$zuluVersion = "11.76.21"
+$jreVersion = "11.0.25"
 
 Write-Host " Done!" -ForegroundColor Green
