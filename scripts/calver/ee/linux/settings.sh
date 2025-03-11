30b9acb9caa7f0ad455426298facccbf6d3b49d7
Tom Geudens <tomgeudens@users.noreply.github.com>
2025-03-11 07:21:41 +0100

    Neo4j Calver EE 2025.02.0

---
+++ scripts/calver/ee/linux/settings.sh
@@ -0,0 +1,48 @@
+. ./scripts/version.sh
+
+# changes to neo4j.conf file
+echo "# Custom - Generic" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.config.strict_validation.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+echo "# Custom - Metrics" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.metrics.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.metrics.filter=*" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.metrics.csv.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.metrics.prometheus.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.metrics.graphite.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.metrics.jmx.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+echo "# Custom - Query Log" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "db.logs.query.enabled=OFF" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+echo "# Custom - Miscellaneous" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.db.timezone=SYSTEM" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.security.procedures.unrestricted=apoc*,gds*" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "browser.remote_content_hostname_whitelist=*" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+echo "# Custom - Memory" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.memory.heap.initial_size=2g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.memory.heap.max_size=2g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.memory.pagecache.size=1g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.memory.transaction.total.max=2000m" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "db.memory.transaction.max=1g" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+echo "# Custom - Network Settings" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "server.default_listen_address=0.0.0.0" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+# there is now a reasonable default for this (after only 5 years of waiting for it ;-)
+# echo "# Custom - Transaction Log" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+# echo "db.tx_log.rotation.retention_policy=1G size" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+echo "# no usage data collection please" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+echo "dbms.usage_report.enabled=false" >> neo4j-enterprise-${neo4jVersion}/conf/neo4j.conf
+
+# create apoc.conf file
+echo "apoc.export.file.enabled=true" > neo4j-enterprise-${neo4jVersion}/conf/apoc.conf
+echo "apoc.import.file.enabled=true" >> neo4j-enterprise-${neo4jVersion}/conf/apoc.conf
+echo "apoc.import.file.use_neo4j_config=true" >> neo4j-enterprise-${neo4jVersion}/conf/apoc.conf
+
+# setting the initial password
+export JAVA_HOME="$(pwd)/jdk-${temurinHomeVersion}-jre"
+export PATH="$JAVA_HOME/bin:$PATH"
+neo4j-enterprise-${neo4jVersion}/bin/neo4j-admin dbms set-initial-password morpheus
---
+++ scripts/calver/ee/linux/unpack.sh
@@ -0,0 +1,5 @@
+. ./scripts/version.sh
+
+tar -xzf install/OpenJDK${javaVersion}U-jre_x64_linux_hotspot_${temurinFileVersion}.tar.gz
+tar -xzf install/neo4j-enterprise-${neo4jVersion}-unix.tar.gz
+unzip -q install/neo4j-graph-data-science-${gdsVersion}.zip -d install
