35d120aadf56b95d3d429d6886b1f1e40a07c999
Tom Geudens <tomgeudens@users.noreply.github.com>
2025-04-16 10:36:24 +0200

    Neo4j Calver EE / CE 2025.03.0, GDS 2.16.0

--- scripts/calver/ce/linux/version.sh
+++ scripts/calver/ce/linux/version.sh
@@ -1,5 +1,5 @@
-export neo4jVersion="2025.02.0"
-export gdsVersion="2.15.0"
+export neo4jVersion="2025.03.0"
+export gdsVersion="2.16.0"
 export javaVersion="21"
 export temurinURLVersion="21.0.6%2B7"
 export temurinFileVersion="21.0.6_7"
--- scripts/calver/ce/windows/version.ps1
+++ scripts/calver/ce/windows/version.ps1
@@ -1,8 +1,8 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "2025.02.0"
+$neo4jVersion = "2025.03.0"
 $neo4jEdition = "community"
-$gdsVersion = "2.15.0"
+$gdsVersion = "2.16.0"
 $javaVersion="21"
 $temurinURLVersion="21.0.6%2B7"
 $temurinFileVersion="21.0.6_7"
