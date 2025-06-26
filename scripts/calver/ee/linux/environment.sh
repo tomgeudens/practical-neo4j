59208ab636cebabda9cc8215693deb34ca7a6d61
Tom Geudens <tomgeudens@users.noreply.github.com>
2025-06-26 09:58:16 +0200

    Add ${neo4jEdition} in scripts

--- scripts/calver/ee/linux/environment.sh
+++ scripts/calver/ee/linux/environment.sh
@@ -1,6 +1,6 @@
 . ./scripts/version.sh
 
-export NEO4J_HOME="$(pwd)/neo4j-enterprise-${neo4jVersion}"
+export NEO4J_HOME="$(pwd)/neo4j-${neo4jEdition}-${neo4jVersion}"
 export JAVA_HOME="$(pwd)/jdk-${temurinHomeVersion}-jre"
 export PATH="$JAVA_HOME/bin:$PATH"
 export NEO4J_ACCEPT_LICENSE_AGREEMENT="yes"
