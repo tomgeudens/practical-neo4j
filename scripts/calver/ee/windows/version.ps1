610cafd9781f67874032f3af79fb42437ead5be5
Tom Geudens <tomgeudens@users.noreply.github.com>
2025-06-26 09:38:05 +0200

    Neo4j Calver EE / CE 2025.05.0, GDS 2.18.0

--- scripts/calver/ee/windows/version.ps1
+++ scripts/calver/ee/windows/version.ps1
@@ -1,8 +1,8 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "2025.04.0"
+$neo4jVersion = "2025.05.0"
 $neo4jEdition = "enterprise"
-$gdsVersion = "2.17.0"
+$gdsVersion = "2.18.0"
 $javaVersion="21"
 $temurinURLVersion="21.0.7%2B6"
 $temurinFileVersion="21.0.7_6"
