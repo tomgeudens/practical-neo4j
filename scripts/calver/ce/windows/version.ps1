cf5df6478cb0b28dbdb82d1920313a3b4eb9b3c0
Tom Geudens <tomgeudens@users.noreply.github.com>
2025-08-27 12:44:31 +0200

    Neo4j Calver EE / CE 2025.07.1, GDS 2.20.0, Java 21.0.8

--- scripts/calver/ce/windows/version.ps1
+++ scripts/calver/ce/windows/version.ps1
@@ -1,11 +1,11 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "2025.06.0"
+$neo4jVersion = "2025.07.1"
 $neo4jEdition = "community"
-$gdsVersion = "2.19.0"
+$gdsVersion = "2.20.0"
 $javaVersion="21"
-$temurinURLVersion="21.0.7%2B6"
-$temurinFileVersion="21.0.7_6"
-$temurinHomeVersion="21.0.7+6"
+$temurinURLVersion="21.0.8%2B9"
+$temurinFileVersion="21.0.8_9"
+$temurinHomeVersion="21.0.8+9"
 
 Write-Host " Done!" -ForegroundColor Green
