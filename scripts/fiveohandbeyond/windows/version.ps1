e98a30dd1c61b3f2eab3b75dc9c8697e6423a15c
Tom Geudens <tom.geudens@hush.ai>
2026-05-02 10:34:44 +0200

    Neo4j 5.26.25, Java 17.0.19 (Linux only)

--- scripts/fiveohandbeyond/windows/version.ps1
+++ scripts/fiveohandbeyond/windows/version.ps1
@@ -1,6 +1,6 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "5.26.24"
+$neo4jVersion = "5.26.25"
 $gdsVersion = "2.13.9"
 $javaVersion="17"
 $temurinURLVersion="17.0.18%2B8"
